9a23ffdf48dd55d8added2b3f1053b3b98b9a245
张谱 <Henry_zp@163.com>
2016-12-13 18:11:01 +0800

    test cli

--- src/js/config/modal_config.js
+++ src/js/config/modal_config.js
@@ -8,41 +8,45 @@ export default {
     padding: 16,
     closeIconClass: "iconfont icon-shanchu5",
     //边界范围
-    boundary: function(elem, parentElem) {
+    boundary(elem, parentElem) {
 
-        let width = elem.offsetWidth,
-            height = elem.offsetHeight;
+        const width = elem.offsetWidth
+
+        const height = elem.offsetHeight
 
         return {
+
             getLeft: function(left) {
 
-                let containerWidth = document.documentElement.clientWidth;
+                const containerWidth = document.documentElement.clientWidth
 
-                if(left < width/2) {
-                    return width/2;
+                if (left < width / 2) {
+                    return width / 2
                 }
 
-                if(left > containerWidth - width/2) {
-                    return containerWidth - width/2;
+                if (left > (containerWidth - width/2)) {
+                    return containerWidth - width/2
                 }
 
-                return left;
+                return left
             },
+
             getTop: function(top) {
 
-                let containerHeight = document.documentElement.clientHeight;
+                const containerHeight = document.documentElement.clientHeight
 
-                if(top < height/2) {
-                    return height/2;
+                if (top < height / 2) {
+                    return height / 2
                 }
 
-                if(top > containerHeight - height/2) {
-                    return containerHeight - height/2;
+                if (top > (containerHeight - height / 2)) {
+                    return containerHeight - height / 2
                 }
 
-                return top;
+                return top
 
             }
+
         }
     }
 }
